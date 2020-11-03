d64e1301cb6cce3fc1517370c79dd7bcc96344d4
Bharat <bharat.gude@twenty70.in>
2020-11-03 15:34:20 +0530

    Initial Commit

---
+++ WebwinTaskTests/WebwinTaskTests.swift
@@ -0,0 +1,33 @@
+//
+//  WebwinTaskTests.swift
+//  WebwinTaskTests
+//
+//  Created by Bharat on 03/11/20.
+//
+
+import XCTest
+@testable import WebwinTask
+
+class WebwinTaskTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
